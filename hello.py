879c3ecacc947e5632b167ec6e5152d41488d607
kohei <e1541068@gmail,com>
2025-07-09 23:01:34 +0900

    初めてのPythonファイルを追加

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Hello, Python!")
+
+print("好きな食べ物")
+print("2+3=", 2 + 3)
+print("こんにちは")
+
+number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for sum1 in number1:
+    if sum1 % 3 == 0:
+        print("アホ" + str(sum1))
+    else:
+        print(sum1)
